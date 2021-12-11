fb169301bc52aef4a75515aedf1755982609d1c6
Jason Zhou <zhenhao@royal-16.cs.wisc.edu>
2021-12-10 21:54:26 -0600

    Added victim capacity test results

--- sim_res/run_O3CPU_l1_victim.sh
+++ sim_res/run_O3CPU_l1_victim.sh
@@ -25,3 +25,5 @@
 ./build/X86/gem5.opt -d DerivO3CPU_M_Dyn_victim_1kB configs/project/victim_cache.py --binary=../bench_tests/M_Dyn/bench.X86 --victim=1 --victim_size=1kB --victim_assoc=16 --cpu_type=DerivO3CPU
 ./build/X86/gem5.opt -d DerivO3CPU_M_Dyn_victim_4kB configs/project/victim_cache.py --binary=../bench_tests/M_Dyn/bench.X86 --victim=1 --victim_size=4kB --victim_assoc=64 --cpu_type=DerivO3CPU
 ./build/X86/gem5.opt -d DerivO3CPU_M_Dyn_no_victim configs/project/victim_cache.py --binary=../bench_tests/M_Dyn/bench.X86 --victim=0 --cpu_type=DerivO3CPU
+
+tar -czf m5out.tar m5out
